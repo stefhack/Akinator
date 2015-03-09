c54e8b14458e4a3bf81524a0e6e409d4e9d92a0b
littlekev34 <littlekev34@hotmail.fr>
2015-03-09 18:23:20 +0100

    creation mainactivity + gameactivity

---
+++ PROJECT/src/ia/epsi/akinator/GameActivity.java
@@ -0,0 +1,21 @@
+package ia.epsi.akinator;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class GameActivity extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_game);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
