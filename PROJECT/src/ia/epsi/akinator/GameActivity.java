6595e5025c6e5a8c02cb78cd922e0e27c19c6d93
unknown <duprestephane@hotmail.fr>
2015-03-23 16:14:27 +0100

    create jsonSingleton

--- PROJECT/src/ia/epsi/akinator/GameActivity.java
+++ PROJECT/src/ia/epsi/akinator/GameActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 
+
 public class GameActivity extends Activity{
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
