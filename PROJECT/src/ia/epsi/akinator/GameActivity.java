4b6c85b73d6eebeedf63d6b692ba54818bc1adaf
littlekev34 <littlekev34@hotmail.fr>
2015-04-20 16:33:57 +0200

    commit pr pull

--- PROJECT/src/ia/epsi/akinator/GameActivity.java
+++ PROJECT/src/ia/epsi/akinator/GameActivity.java
@@ -7,11 +7,13 @@ import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
 
 
 public class GameActivity extends Activity{
 	//Declaration
-	Button ButtonTest;
+	Button ButtonTest, ButtonYes,ButtonNo, ButtonDontNo, ButtonRather, ButtonRatherNot;
+	TextView TextViewQuestion;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +22,18 @@ public class GameActivity extends Activity{
 		
 		//Assignement
 		ButtonTest = (Button)findViewById(R.id.buttonTest);
+		ButtonYes = (Button)findViewById(R.id.buttonYes);
+		ButtonNo = (Button)findViewById(R.id.buttonNo);
+		ButtonDontNo = (Button)findViewById(R.id.buttonDontNo);
+		ButtonRather = (Button)findViewById(R.id.buttonRather);
+		ButtonRatherNot = (Button)findViewById(R.id.buttonRatherNot);
+		TextViewQuestion = (TextView)findViewById(R.id.textViewQuestionRequest);
+		
+		
+		//Call first question
+		
+		TextViewQuestion.setText("Coucou chez moi");
+		
         
 		
 		//Button click
@@ -28,6 +42,41 @@ public class GameActivity extends Activity{
         	public void onClick(View v) {
         		Intent intent=new Intent(GameActivity.this,ResultActivity.class);
     			startActivity(intent);
+        	}
+        });
+		
+		ButtonYes.setOnClickListener(new OnClickListener() {
+        	@Override
+        	public void onClick(View v) {
+        		
+        	}
+        });
+		
+		ButtonNo.setOnClickListener(new OnClickListener() {
+        	@Override
+        	public void onClick(View v) {
+        		
+        	}
+        });
+		
+		ButtonDontNo.setOnClickListener(new OnClickListener() {
+        	@Override
+        	public void onClick(View v) {
+        		
+        	}
+        });
+		
+		ButtonRather.setOnClickListener(new OnClickListener() {
+        	@Override
+        	public void onClick(View v) {
+        		
+        	}
+        });
+		
+		ButtonRatherNot.setOnClickListener(new OnClickListener() {
+        	@Override
+        	public void onClick(View v) {
+        		
         	}
         });
 	}
