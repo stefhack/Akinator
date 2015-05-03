f0217fac6a270db92d2e7ddaadd9e07a6fdd7ea9
JohnnyBeGood34 <jonathanaffre@gmail.com>
2015-05-03 12:31:44 +0200

    Limitation sur le je sais pas

--- PROJECT/src/ia/epsi/akinator/GameActivity.java
+++ PROJECT/src/ia/epsi/akinator/GameActivity.java
@@ -21,145 +21,171 @@ import Management.Algorithm;
 import Management.JsonReader;
 import Management.JsonSingleton;
 
-public class GameActivity extends Activity{
+public class GameActivity extends Activity {
 	private static Context gameContext;
-	//Declaration
-	private Button buttonYes,buttonNo, buttonDontNo, buttonRather, buttonRatherNot;
+	// Declaration
+	private Button buttonYes, buttonNo, buttonDontNo, buttonRather,
+			buttonRatherNot;
 	private TextView textViewQuestion;
 	private String actualQuestion;
 	private String actualKey;
-	private HashMap<String,String> hashMapQuestionResponse = new HashMap<String, String>();
-	private Algorithm algo ;
-    private JsonSingleton jsonSingleton;
-    private int nb_questions_asked =0;
-    private JsonReader jsonReader;
+	private HashMap<String, String> hashMapQuestionResponse = new HashMap<String, String>();
+	private Algorithm algo;
+	private JsonSingleton jsonSingleton;
+	private int nb_questions_asked = 0;
+	private JsonReader jsonReader;
 	ImageView imageViewGenie;
+	boolean isAlreadyIDontKnow = true;
 	Integer countForGeniePicture = 0;
+	private int iDontknowCounter = 1;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		this.gameContext = getApplicationContext();
-        jsonSingleton=JsonSingleton.getInstance(gameContext);
-        jsonReader=new JsonReader(gameContext);
+		GameActivity.gameContext = getApplicationContext();
+		jsonSingleton = JsonSingleton.getInstance(gameContext);
+		jsonReader = new JsonReader(gameContext);
 		setContentView(R.layout.activity_game);
-		
 
-        algo = new Algorithm(gameContext);
+		algo = new Algorithm(gameContext);
 		algo.resetAllData();
-		
+
 		// Reset the JSON's with values from start
 		jsonSingleton.initializeJSONs();
-		//Assignement
-		this.buttonYes = (Button)findViewById(R.id.buttonYes);
-		this.buttonNo = (Button)findViewById(R.id.buttonNo);
-		this.buttonDontNo = (Button)findViewById(R.id.buttonDontNo);
-		this.buttonRather = (Button)findViewById(R.id.buttonRather);
-		this.buttonRatherNot = (Button)findViewById(R.id.buttonRatherNot);
-		this.textViewQuestion = (TextView)findViewById(R.id.textViewQuestionRequest);
-		this.imageViewGenie = (ImageView)findViewById(R.id.imageViewGenie);
+		// Assignement
+		this.buttonYes = (Button) findViewById(R.id.buttonYes);
+		this.buttonNo = (Button) findViewById(R.id.buttonNo);
+		this.buttonDontNo = (Button) findViewById(R.id.buttonDontNo);
+		this.buttonRather = (Button) findViewById(R.id.buttonRather);
+		this.buttonRatherNot = (Button) findViewById(R.id.buttonRatherNot);
+		this.textViewQuestion = (TextView) findViewById(R.id.textViewQuestionRequest);
+		this.imageViewGenie = (ImageView) findViewById(R.id.imageViewGenie);
 
-        //TESTS
-        {
-            try {
-                Log.i("GAME ACTIVITY READ JSON persos FROM INTERNAL STORAGE :", jsonReader.readJSONfromInternalStorage("personnages.json"));
-                Log.i("GAME ACTIVITY READ JSON questions FROM INTERNAL STORAGE :", jsonReader.readJSONfromInternalStorage("questions.json"));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+		// TESTS
+		{
+			try {
+				Log.i("GAME ACTIVITY READ JSON persos FROM INTERNAL STORAGE :",
+						jsonReader
+								.readJSONfromInternalStorage("personnages.json"));
+				Log.i("GAME ACTIVITY READ JSON questions FROM INTERNAL STORAGE :",
+						jsonReader
+								.readJSONfromInternalStorage("questions.json"));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 		displayQuestion();
 
-
-
-
-		
 		this.buttonDontNo.setOnClickListener(new OnClickListener() {
-        	@Override
-        	public void onClick(View v) {
-                takeResponseAndGoOn("0");
-        	}
-        });
-		
+			@Override
+			public void onClick(View v) {
+				takeResponseAndGoOn("0");
+				if (!isAlreadyIDontKnow) {
+					isAlreadyIDontKnow = true;
+				} else {
+					iDontknowCounter++;
+				}
+			}
+		});
+
 		this.buttonYes.setOnClickListener(new OnClickListener() {
-        	@Override
-        	public void onClick(View v) {
-                takeResponseAndGoOn("1");
-        	}
-        });
-		
+			@Override
+			public void onClick(View v) {
+				takeResponseAndGoOn("1");
+				isAlreadyIDontKnow = false;
+				iDontknowCounter = 0;
+			}
+		});
+
 		this.buttonNo.setOnClickListener(new OnClickListener() {
-        	@Override
-        	public void onClick(View v) {
-                takeResponseAndGoOn("2");
-        	}
-        });
-		
+			@Override
+			public void onClick(View v) {
+				takeResponseAndGoOn("2");
+				isAlreadyIDontKnow = false;
+				iDontknowCounter = 0;
+			}
+		});
+
 		this.buttonRather.setOnClickListener(new OnClickListener() {
-        	@Override
-        	public void onClick(View v) {
-                takeResponseAndGoOn("3");
-        	}
-        });
-		
+			@Override
+			public void onClick(View v) {
+				takeResponseAndGoOn("3");
+				isAlreadyIDontKnow = false;
+				iDontknowCounter = 0;
+			}
+		});
+
 		this.buttonRatherNot.setOnClickListener(new OnClickListener() {
-        	@Override
-        	public void onClick(View v) {
-                takeResponseAndGoOn("4");
-        	}
-        });
+			@Override
+			public void onClick(View v) {
+				takeResponseAndGoOn("4");
+				isAlreadyIDontKnow = false;
+				iDontknowCounter = 0;
+			}
+		});
 	}
 
+	private void takeResponseAndGoOn(String responseCode) {
+		// On incrémente le nb de questions posées
+		++nb_questions_asked;
 
-    private void takeResponseAndGoOn(String responseCode){
-        //On incrémente le nb de questions posées
-        ++nb_questions_asked;
+		// Remplit la liste des questions avec les réponses données
+		fillHashMapQuestionResponse(responseCode);
 
-        //Remplit la liste des questions avec les réponses données
-        fillHashMapQuestionResponse(responseCode);
+		// Calcule du score pour chaque perso pour la question courante
+		// avec la réponse donnée par le USer
 
-        //Calcule du score pour chaque perso pour la question courante
-        // avec la réponse donnée par le USer
+		try {
+			algo.calculateScoreForCharacters(this.actualKey, responseCode);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
 
-        try {
-            algo.calculateScoreForCharacters(this.actualKey,responseCode);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+		// Suppression de la dernière question posée
+		try {
 
+			algo.deleteQuestion(this.actualKey);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
 
-        //Suppression de la dernière question posée
-        try {
+		// On a pas atteint le minimum de questions à poser
+		// OU il n'y a pas encore de persos à proposer
+		// ET s'il y a encore des questions
+		try {
+			if (iDontknowCounter < 5) {
+				if (nb_questions_asked < algo.QUESTIONS_THRESOLD
+						|| !algo.hasMorePersoToPropose()
+						&& jsonSingleton.getQuestionsLeft() > 0) {
+					Log.i("I DONT KNOW COUNTER MOTHER FUCKER",
+							String.valueOf(iDontknowCounter));
+					// On pose à nouveau une question
+					displayQuestion();
+				}
+				// On commence à proposer un personnage
+				else {
+					goToResultActivity();
+				}
+			} else {
+				goToResultActivity();
+			}
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
 
-            algo.deleteQuestion(this.actualKey);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+	}
 
-
-        //On a pas atteint le minimum de questions à poser
-        //OU il n'y a pas encore de persos à proposer
-        //ET  s'il y a encore des questions
-        try {
-            if(nb_questions_asked < algo.QUESTIONS_THRESOLD || !algo.hasMorePersoToPropose() && jsonSingleton.getQuestionsLeft() > 0){
-                //On pose à nouveau une question
-                displayQuestion();
-            }
-            //On commence à proposer un personnage
-            else{
-                Intent intent=new Intent(GameActivity.this,ResultActivity.class);
-                intent.putExtra("responses",this.hashMapQuestionResponse);//on fait passer les réponses à la prochaine activité
-                int[] nbQuestionArray = new int[1];
-                nbQuestionArray[0] = nb_questions_asked;
-                intent.putExtra("nbQuestionsAsked",nbQuestionArray);//on fait passer le nombre de questions pour calculer le score
-                startActivity(intent);
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-    }
-
+	private void goToResultActivity(){
+		Intent intent = new Intent(GameActivity.this,
+				ResultActivity.class);
+		intent.putExtra("iDontKnowCounter", iDontknowCounter);
+		intent.putExtra("responses", this.hashMapQuestionResponse);
+		int[] nbQuestionArray = new int[1];
+		nbQuestionArray[0] = nb_questions_asked;
+		intent.putExtra("nbQuestionsAsked", nbQuestionArray);
+		startActivity(intent);
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -167,43 +193,40 @@ public class GameActivity extends Activity{
 		return true;
 	}
 
-	public static Context getGameContext(){
+	public static Context getGameContext() {
 		return gameContext;
 	}
-	
-	private void fillHashMapQuestionResponse(String response){
+
+	private void fillHashMapQuestionResponse(String response) {
 		this.hashMapQuestionResponse.put(this.actualKey, response);
 	}
-	
-	private void displayQuestion(){
-		showGeniePicture();
-        String requestAlgorithm = "";
-        try {
 
-        	requestAlgorithm=algo.getTheMostPertinenteQuestion();
+	private void displayQuestion() {
+		showGeniePicture();
+		String requestAlgorithm = "";
+		try {
+
+			requestAlgorithm = algo.getTheMostPertinenteQuestion();
 		} catch (JSONException e) {
 
 			e.printStackTrace();
 		}
-        
-        
-        String[] partsRequestAlgorithm = requestAlgorithm.split(";");
-        this.actualKey = partsRequestAlgorithm[0];
-        this.actualQuestion = partsRequestAlgorithm[1];
-        this.textViewQuestion.setText(actualQuestion);
+
+		String[] partsRequestAlgorithm = requestAlgorithm.split(";");
+		this.actualKey = partsRequestAlgorithm[0];
+		this.actualQuestion = partsRequestAlgorithm[1];
+		this.textViewQuestion.setText(actualQuestion);
 	}
-	
-	private void showGeniePicture(){
-		if(countForGeniePicture == 0){
+
+	private void showGeniePicture() {
+		if (countForGeniePicture == 0) {
 			countForGeniePicture = 3;
 		}
-		if(countForGeniePicture == 1){
+		if (countForGeniePicture == 1) {
 			imageViewGenie.setImageResource(R.drawable.genie1);
-		}
-		else if(countForGeniePicture == 2){
+		} else if (countForGeniePicture == 2) {
 			imageViewGenie.setImageResource(R.drawable.genie2);
-		}
-		else if(countForGeniePicture == 3) {
+		} else if (countForGeniePicture == 3) {
 			imageViewGenie.setImageResource(R.drawable.genie3);
 		}
 		countForGeniePicture--;
--- PROJECT/src/ia/epsi/akinator/ResultActivity.java
+++ PROJECT/src/ia/epsi/akinator/ResultActivity.java
@@ -14,72 +14,93 @@ import java.util.HashMap;
 
 import Management.Algorithm;
 
-public class ResultActivity extends Activity{
-	//Declaration
+public class ResultActivity extends Activity {
+	// Declaration
 	Button buttonYes, buttonNo;
-	TextView resultPerso,textViewResponse;
-    Algorithm algo;
-    private static Context gameContext;
-    private HashMap<String,String> hashMapQuestionResponse = new HashMap<String, String>();
-    private double nb_questions_asked;
+	TextView resultPerso, textViewResponse;
+	Algorithm algo;
+	private static Context gameContext;
+	private HashMap<String, String> hashMapQuestionResponse = new HashMap<String, String>();
+	private double nb_questions_asked;
+	private int iDontKnowCounter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_result);
-        Intent intent = getIntent();
-        hashMapQuestionResponse=(HashMap<String,String>)intent.getSerializableExtra("responses");
-        nb_questions_asked= (double)intent.getIntArrayExtra("nbQuestionsAsked")[0];
-        algo=new Algorithm(getApplicationContext());
+		Intent intent = getIntent();
+		hashMapQuestionResponse = (HashMap<String, String>) intent
+				.getSerializableExtra("responses");
+		nb_questions_asked = (double) intent
+				.getIntArrayExtra("nbQuestionsAsked")[0];
+		algo = new Algorithm(getApplicationContext());
+		iDontKnowCounter = intent.getIntExtra("iDontKnowCounter", 0);
+		// Assignement
+		buttonYes = (Button) findViewById(R.id.buttonYes);
+		buttonNo = (Button) findViewById(R.id.buttonNo);
+		resultPerso = (TextView) findViewById(R.id.textViewResult);
+		textViewResponse = (TextView) findViewById(R.id.textViewResponse);
+		// On affiche la première proposition
+		showNextProposition();
 
-		//Assignement
-		buttonYes = (Button)findViewById(R.id.buttonYes);
-		buttonNo = (Button)findViewById(R.id.buttonNo);
-        resultPerso = (TextView)findViewById(R.id.textViewResult);
-        textViewResponse = (TextView)findViewById(R.id.textViewResponse);
-        //On affiche la première proposition
-        showNextProposition();
-
-		//Button click
+		// Button click
 		buttonYes.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(ResultActivity.this, EndGameActivity.class);
-                startActivity(intent);
-            }
-        });
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(ResultActivity.this,
+						EndGameActivity.class);
+				startActivity(intent);
+			}
+		});
 
 		buttonNo.setOnClickListener(new OnClickListener() {
-        	@Override
-        	public void onClick(View v) {
+			@Override
+			public void onClick(View v) {
 
-                algo.deletePersoFromScore((String) resultPerso.getText());
-                goOnProposition();
-        	}
-        });
+				algo.deletePersoFromScore((String) resultPerso.getText());
+				goOnProposition();
+			}
+		});
 	}
 
+	/*
+	 * Continue d'afficher des propositions pertinentes
+	 */
+	private void goOnProposition() {
 
-    /*
-    * Continue d'afficher des propositions  pertinentes
-    * */
-    private void goOnProposition(){
+		if (algo.hasMorePersoToPropose())
+			showNextProposition();
 
-        if(algo.hasMorePersoToPropose()) showNextProposition();
+		else { // Il n' y a plus de propositions à faire, on demande à
+				// l'utilisateur
+				// à quel personnage il pensait
+			Intent intent = new Intent(ResultActivity.this,
+					LearnCharacterActivity.class);
+			intent.putExtra("responses", this.hashMapQuestionResponse);// on
+																		// fait
+																		// passer
+																		// les
+																		// réponses
+																		// à la
+																		// prochaine
+																		// activité
+			startActivity(intent);
+		}
+	}
 
-        else { //Il n' y a plus de propositions à faire, on demande à l'utilisateur
-            // à quel personnage il pensait
-            Intent intent=new Intent(ResultActivity.this,LearnCharacterActivity.class);
-            intent.putExtra("responses",this.hashMapQuestionResponse);//on fait passer les réponses à la prochaine activité
-            startActivity(intent);
-        }
-    }
-
-    /*Affichage de la proposition */
-    private void showNextProposition(){
-    	textViewResponse.setText("Je suis sûr à "+algo.getMaxScore(nb_questions_asked)+" % que vous pensiez �:");
-        resultPerso.setText(algo.getPersoByMaxScore());
-    }
+	/* Affichage de la proposition */
+	private void showNextProposition() {
+		if (iDontKnowCounter == 5) {
+			textViewResponse.setText("Je suis sur � 100%");
+			resultPerso.setText("Celui qui ne savait jamais rien!");
+			buttonNo.setVisibility(View.INVISIBLE);
+		} else {
+			textViewResponse.setText("Je suis sûr à "
+					+ algo.getMaxScore(nb_questions_asked)
+					+ " % que vous pensiez �:");
+			resultPerso.setText(algo.getPersoByMaxScore());
+		}
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
